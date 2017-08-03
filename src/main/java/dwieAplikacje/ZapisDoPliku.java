99b306906b5ce09b2f824ac941d535b0cdadd95c
DorotaLeniec <dorleniec@gmail.com>
2017-08-03 20:50:59 +0200

    metoda SET_FILE przy zapisie pliku

--- src/main/java/dwieAplikacje/ZapisDoPliku.java
+++ src/main/java/dwieAplikacje/ZapisDoPliku.java
@@ -11,16 +11,25 @@ public class ZapisDoPliku {
         File sharedFile = new File("shared.txt");
         Scanner sc = new Scanner(System.in);
 
-        try(PrintWriter writer = new PrintWriter(new FileOutputStream(sharedFile,true))){
+        try (PrintWriter writer = new PrintWriter(new FileOutputStream(sharedFile))) {
             System.out.println("Otworzono plik sharedFile.txt.");
             System.out.println("Rozpocznij pisanie albo nacisnij 'q' aby wyjsc");
             String line = sc.nextLine();
-            while(!line.equals("q")){
-                writer.println(line);
-                writer.flush();
-                line = sc.nextLine();
+            while (!line.equals("q")) {
+                if (line.equals("SET_FILE")) {
+                    System.out.println("Podaj nazwe pliku:");
+                    String fileName = sc.next();
+//                    sharedFile = new File(fileName);
+//                    writer.close();
+//                    writer = new PrintWriter(new FileOutputStream(sharedFile));
+                    line = "";
+                } else {
+                    writer.println(line);
+                    writer.flush();
+                    line = sc.nextLine();
+                }
             }
-        }catch (FileNotFoundException fnfe){
+        } catch (FileNotFoundException fnfe) {
             System.out.println("File Not Found.");
         }
     }
