167c868c32d06060190686da855c2d1f5ff475cb
DorotaLeniec <dorleniec@gmail.com>
2017-08-07 18:03:48 +0200

    Poprawienie metod magazynu

--- src/main/java/shop/Magazine.java
+++ src/main/java/shop/Magazine.java
@@ -8,23 +8,34 @@ import java.util.*;
 public class Magazine {
     private Map<PRODUCT_CLASS,List<Product>> productsMap = new HashMap<>();
 
-    public Magazine(){
-        productsMap.put(PRODUCT_CLASS.HIGH, new ArrayList<>());
-        productsMap.put(PRODUCT_CLASS.MEDIUM, new ArrayList<>());
-        productsMap.put(PRODUCT_CLASS.LOW, new ArrayList<>());
-    }
 
     void addProduct(String name, double price, PRODUCT_TYPE type, PRODUCT_CLASS prodClass){
-        productsMap.get(prodClass).add(new Product(prodClass,type,name, price));
+        Product product = new Product(prodClass,type,name,price);
+        if(productsMap.containsKey(prodClass)){
+            productsMap.get(prodClass).add(product);
+
+        }else {
+            List<Product> newProductList = new LinkedList<>();
+            newProductList.add(product);
+            productsMap.put(prodClass,newProductList);
+        }
         System.out.println("Dodano produkt");
     }
 
-    void showProductByClass(PRODUCT_CLASS prodClass){
+    void printProductByClass(PRODUCT_CLASS prodClass){
         System.out.println(productsMap.get(prodClass));
     }
 
-    void showProductByType(PRODUCT_TYPE prodType){
-        List<Product> products = new ArrayList<>();
+    void printProductByType(PRODUCT_TYPE prodType){
+        List<Product> filteredProducts = new ArrayList<>();
+        for(List<Product> list : productsMap.values()){
+            for(Product product : list){
+                if(product.getProdType() == prodType){
+                    filteredProducts.add(product);
+                }
+            }
+        }
+        System.out.println(filteredProducts);
 
     }
 
